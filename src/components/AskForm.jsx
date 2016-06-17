0adf1ab446d426fa89e287e5e7d1b7f11c7c29e3
Allan Hortle <allan.hortle@blueflag.com.au>
2016-06-17 14:46:50 +1000

    remove some userstore refs

--- src/components/AskForm.jsx
+++ src/components/AskForm.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
-import UserStore from 'trc-client-core/src/user/UserStore';
+import {childContextTypes} from 'trc-client-core/src/auth/AuthProvider';
 
-var AskForm = React.createClass({
-    displayName: 'AskForm',
-    render: function () {
-        return (
-            <section>
-                <form id="ask_us" method="post" action="/help/ask">
-                    <h3 className="t-capitalize">Ask {UserStore.get('site').toLowerCase()} a question</h3>
-                    <input type="hidden" name="firstName" id="firstName" value={UserStore.get('firstName')}/>
-                    <input type="hidden" name="lastName" id="lastName" value={UserStore.get('lastName')}/>
-                    <textarea className="Textarea row-x2" name="message" id="message" required="required"></textarea>
-                    <button type="submit" className="Button">Ask</button>
-                    <a href="/help/faq" className="l-right readmore">Check the FAQ</a>
-                </form>
-            </section>
-        );
-    }
-});
+export default function AskForm(props, {currentUser}) {
+    return <section>
+        <form id="ask_us" method="post" action="/help/ask">
+            <h3 className="t-capitalize">Ask us a question</h3>
+            <input type="hidden" name="firstName" id="firstName" value={currentUser.get('firstName')}/>
+            <input type="hidden" name="lastName" id="lastName" value={currentUser.get('lastName')}/>
+            <textarea className="Textarea row-x2" name="message" id="message" required="required"></textarea>
+            <button type="submit" className="Button">Ask</button>
+            <a href="/help/faq" className="l-right readmore">Check the FAQ</a>
+        </form>
+    </section>;
+}
 
-module.exports = AskForm;
+AskForm.contextTypes = childContextTypes;
