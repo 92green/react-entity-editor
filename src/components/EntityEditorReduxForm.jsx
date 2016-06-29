8e94e180c450a3ae508f0c71ffe329e2c974867c
Damien Clarke <dxinteractive@gmail.com>
2016-06-29 10:05:15 +1000

    EntityEditorReduxForm now a HOC

--- src/components/EntityEditorReduxForm.jsx
+++ src/components/EntityEditorReduxForm.jsx
@@ -8,203 +8,196 @@ import Button from 'toyota-styles/lib/components/Button';
 import ErrorList from 'trc-client-core/src/components/ErrorList';
 
 //
-// EntityEditorReduxForm class
+// EntityEditorReduxForm higher order component
 //
 // Provides a redux form shell that works with EntityEditor
-// Give it child props of your form fields
+// Use it like you would use the reduxForm higher order component
 //
 
-class EntityEditorReduxForm extends Component {
+export default (reduxFormConfig) => (ComposedComponent) => {
 
-    setCurrentFormDataAsDefault() {
-        const {
-            formName,
-            values,
-            fields,
-            dispatch
-        } = this.props;
-        dispatch(initialize(formName, values, fromJS(fields).keySeq().toJS()));
-    }
+    class EntityEditorReduxForm extends Component {
 
-    //
-    // event and click handlers
-    //
-
-    handleSubmitForm(values) {
-        this.props
-            .onSave(values)
-            .then(
-                (data) => this.setCurrentFormDataAsDefault(),
-                (err) => {}
-            );
-    }
-
-    handleCloseClick(ee) {
-        this.props
-            .onClose(this.props.dirty)
-            .then(
-                (data) => {},
-                (err) => {}
-            ); 
-    }
-
-    handleResetClick(ee) {
-        if(this.props.onReset) {
-            // if an onReset prop has been supplied, then call it and wait for the promise to return to actually reset the form
-            this.props
-                .onReset()
-                .then(
-                    (data) => this.props.resetForm(),
-                    (err) => Promise.resolve()
-                );
-        } else {
-            // or else just reset the form
-            this.props.resetForm();
+        setCurrentFormDataAsDefault() {
+            const {
+                values,
+                fields,
+                dispatch
+            } = this.props;
+            dispatch(initialize(reduxFormConfig.form, values, fromJS(fields).keySeq().toJS()));
         }
-    }
 
-    handleDeleteClick(ee) {
-        this.props
-            .onDelete()
-            .then(
-                (data) => {},
-                (err) => {}
-            ); 
-    }
+        //
+        // event and click handlers
+        //
 
-    //
-    // render
-    //
+        handleSubmitForm(values) {
+            this.props
+                .onSave(values)
+                .then(
+                    (data) => this.setCurrentFormDataAsDefault(),
+                    (err) => {}
+                );
+        }
 
-    render() {
-        const {
-            // react props
-            children,
-            // own props
-            topButtons,
-            bottomButtons,
-            // props required for redux form
-            fields,
-            // props provided by redux form
-            handleSubmit,
-            errors,
-            // props from entity editor - callbacks
-            onSave,
-            onClose,
-            onDelete,
-            onReset,
-            // props from entity editor - data
-            fetching,
-            willCreateNew
+        handleCloseClick(ee) {
+            this.props
+                .onClose(this.props.dirty)
+                .then(
+                    (data) => {},
+                    (err) => {}
+                ); 
+        }
 
-        } = this.props;
+        handleResetClick(ee) {
+            if(this.props.onReset) {
+                // if an onReset prop has been supplied, then call it and wait for the promise to return to actually reset the form
+                this.props
+                    .onReset()
+                    .then(
+                        (data) => this.props.resetForm(),
+                        (err) => Promise.resolve()
+                    );
+            } else {
+                // or else just reset the form
+                this.props.resetForm();
+            }
+        }
 
-        const propsToAddToChildren = {
-            fields,
-            errors,
-            willCreateNew
-        };
+        handleDeleteClick(ee) {
+            this.props
+                .onDelete()
+                .then(
+                    (data) => {},
+                    (err) => {}
+                ); 
+        }
 
-        const childrenWithProps = React.Children.map(children, (child) => React.cloneElement(child, propsToAddToChildren));
+        //
+        // render
+        //
 
-        return (
-            <form onSubmit={handleSubmit(this.handleSubmitForm.bind(this))}>
-                {topButtons && this.renderButtons()}
-                {childrenWithProps}
-                {bottomButtons && this.renderButtons()}
-            </form>
-        );
-    }
+        render() {
+            const {
+                // props from entity editor - abilities
+                willCreateNew,
+                // own props
+                topButtons,
+                bottomButtons,
+                // props required for redux form
+                fields,
+                // props provided by redux form
+                handleSubmit,
+                errors
 
-    renderButtons() {
-        const {
-            // props provided by redux form
-            dirty,
-            // props from entity editor - data transation states
-            deleting,
-            saving,
-            fetching,
-            // props from entity editor - abilities
-            canSave,
-            canDelete
+            } = this.props;
 
-        } = this.props;
+            return (
+                <form onSubmit={handleSubmit(this.handleSubmitForm.bind(this))}>
+                    {topButtons && this.renderButtons()}
+                    <ComposedComponent
+                        {...this.props}
+                        fields={fields}
+                        willCreateNew={willCreateNew}
+                    />
+                    {bottomButtons && this.renderButtons()}
+                </form>
+            );
+        }
 
-        const canReset = dirty && !fetching;
+        renderButtons() {
+            const {
+                // props provided by redux form
+                dirty,
+                // props from entity editor - data transation states
+                deleting,
+                saving,
+                fetching,
+                // props from entity editor - abilities
+                canSave,
+                canDelete
 
-        return (
-            <div className="t-right margin-bottom">
+            } = this.props;
 
-                {canReset &&
-                    <span>
+            const canReset = dirty && !fetching;
+
+            return (
+                <div className="t-right margin-bottom">
+
+                    {canReset &&
+                        <span>
+                            <Button
+                                modifier="clear"
+                                className="margin-right05"
+                                onClick={this.handleResetClick.bind(this)}>Reset</Button>
+                        </span>
+                    }
+
+                    {canDelete &&
                         <Button
                             modifier="clear"
                             className="margin-right05"
-                            onClick={this.handleResetClick.bind(this)}>Reset</Button>
-                    </span>
-                }
+                            onClick={this.handleDeleteClick.bind(this)}>
+                            {deleting ? "Deleting" : "Delete"}
+                        </Button>
+                    }
 
-                {canDelete &&
                     <Button
-                        modifier="clear"
+                        modifier="grey"
                         className="margin-right05"
-                        onClick={this.handleDeleteClick.bind(this)}>
-                        {deleting ? "Deleting" : "Delete"}
+                        onClick={this.handleCloseClick.bind(this)}
+                    >Close</Button>
+
+                    <Button
+                        modifier="edit"
+                        type="submit"
+                        disabled={!canSave}>
+                        {saving ? "Saving" : "Save"}
                     </Button>
-                }
 
-                <Button
-                    modifier="grey"
-                    className="margin-right05"
-                    onClick={this.handleCloseClick.bind(this)}
-                >Close</Button>
-
-                <Button
-                    modifier="edit"
-                    type="submit"
-                    disabled={!canSave}>
-                    {saving ? "Saving" : "Save"}
-                </Button>
-
-            </div>
-        );
+                </div>
+            );
+        }
     }
-}
 
-EntityEditorReduxForm.propTypes = {
-    // own props
-    topButtons: PropTypes.bool,
-    bottomButtons: PropTypes.bool,
-    // props required for redux form
-    form: PropTypes.string, // isRequired, but react never seems to recognise this
-    fields: PropTypes.any.isRequired,
-    validate: PropTypes.func,
-    // props from entity editor - callbacks
-    onSave: PropTypes.func.isRequired,
-    onClose: PropTypes.func.isRequired,
-    onDelete: PropTypes.func.isRequired,
-    onReset: PropTypes.func,
-    // props from entity editor - data transaction states
-    reading: PropTypes.bool,
-    creating: PropTypes.bool,
-    updating: PropTypes.bool,
-    deleting: PropTypes.bool,
-    saving: PropTypes.bool,
-    fetching: PropTypes.bool,
-    // props from entity editor - abilities
-    willCopy: PropTypes.bool,
-    canSave: PropTypes.bool,
-    canDelete: PropTypes.bool
-};
-
-EntityEditorReduxForm.defaultProps = {
-    topButtons: false,
-    bottomButtons: true
-};
-
-export default reduxForm({},
-(state, props) => {
-    return {
-        initialValues: props.initialValues
+    EntityEditorReduxForm.propTypes = {
+        // props from entity editor - abilities
+        id: PropTypes.any,
+        willCopy: PropTypes.bool,
+        willCreateNew: PropTypes.bool,
+        canSave: PropTypes.bool,
+        canDelete: PropTypes.bool,
+        // props from entity editor - data transaction states
+        reading: PropTypes.bool,
+        creating: PropTypes.bool,
+        updating: PropTypes.bool,
+        deleting: PropTypes.bool,
+        saving: PropTypes.bool,
+        fetching: PropTypes.bool,
+        // props from entity editor - callbacks
+        onSave: PropTypes.func.isRequired,
+        onClose: PropTypes.func.isRequired,
+        onDelete: PropTypes.func.isRequired,
+        onReset: PropTypes.func,
+        // props required for redux form
+        form: PropTypes.string, // isRequired, but react never seems to recognise this
+        fields: PropTypes.any.isRequired,
+        validate: PropTypes.func,
+        initialValues: PropTypes.object,
+        // own props
+        topButtons: PropTypes.bool,
+        bottomButtons: PropTypes.bool
     };
-})(EntityEditorReduxForm);
+
+    EntityEditorReduxForm.defaultProps = {
+        topButtons: false,
+        bottomButtons: true
+    };
+
+    return reduxForm(reduxFormConfig, (state, props) => {
+        return {
+            initialValues: props.initialValues
+        };
+    })(EntityEditorReduxForm);
+};
+
