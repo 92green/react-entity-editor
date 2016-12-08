94a06e6cc11e581ba506cb9485cc0b82711206e6
Damien Clarke <dxinteractive@gmail.com>
2016-12-08 12:33:10 +1100

    Remove outdated files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import EntityEditor from './EntityEditor';
 import EntityEditorItem from './EntityEditorItem';
 import EntityEditorList from './EntityEditorList';
-import EntityEditorDefault from './EntityEditorDefault'; // todo remove
 import EntityEditorRouter, {createEditorRoutes} from './EntityEditorRouter';
+import {mergeConfig} from './Config';
 
 export {
-  EntityEditor,
   EntityEditorItem,
   EntityEditorList,
-  EntityEditorDefault,
   EntityEditorRouter,
-  createEditorRoutes
+  createEditorRoutes,
+  mergeConfig
 }
