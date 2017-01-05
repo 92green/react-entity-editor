25350bb0c74df662e201024d73dbc47f9418f37d
Damien Clarke <dxinteractive@gmail.com>
2017-01-05 15:37:04 +1100

    Export modal and modal content

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import EntityEditorLoader from './EntityEditorLoader';
 import createEditorRoutes from './router/createEditorRoutes';
 import EntityEditorRoute from './router/EntityEditorRoute';
 import EntityEditorItemRoute from './router/EntityEditorItemRoute';
+import Modal from './Modal';
+import ModalContent from './ModalContent';
 import {mergeConfig} from './Config';
 //import PropChangeHock from '../node_modules/stampy/lib/hock/PropChangeHock';
 
@@ -13,6 +15,8 @@ export {
   EntityEditorLoader,
   EntityEditorRoute,
   EntityEditorItemRoute,
+  Modal,
+  ModalContent,
   createEditorRoutes,
   mergeConfig
   //PropChangeHock
